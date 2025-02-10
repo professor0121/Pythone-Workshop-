eae80bbd845c41088afec440a682e977ef1c814b
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-02-10 12:36:51 +0530

    intiall commit

---
+++ Day01/pyPrectice01.py
@@ -0,0 +1,8 @@
+print("welcome to gyan sagar collage of engineering")
+print("we are working on python programming")
+
+print("statement 1")
+print("statement 2")
+print("statement 3" ,"\t", end ="")
+print("statement 4")
+print("statement 4")
