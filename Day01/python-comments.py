6180f27dc2c8d901a4a2b815418cce38f59a2747
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-02-10 12:39:47 +0530

    python comment

---
+++ Day01/python-comments.py
@@ -0,0 +1,14 @@
+#python comments 
+'''
+hello i am a comment
+i am not excuted when program is running
+'''
+print("welcome to gyan sagar collage of engineering")
+print("we are working on python programming")
+
+print("statement 1")
+print("statement 2")
+print("statement 3" ,"\t", end ="")
+print("statement 4")
+print("statement 4")
+
