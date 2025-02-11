beac2eda7e635ba9db1880691dd8602a3c4db0cc
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-02-11 12:08:59 +0530

    slice are done

---
+++ Day02/sliceInLists.py
@@ -0,0 +1,14 @@
+l=list=[1,2,3,4,5,6,7,8,9]
+ 
+length=len(list)
+
+print('l',l)
+print('l[2] :',l[2])
+print('l[-2] :',l[-2])
+print('l[2:8] :',l[2:8])
+print('l[2:] :',l[2:])
+print('l[::] :',l[::])
+print('l[2] :',l[2])
+print('l[2:8:2] :',l[2:8:2])
+print('l[::-1] :',l[::-1])
+print('l[-6:-2:2] :',l[-6:-2:2])
